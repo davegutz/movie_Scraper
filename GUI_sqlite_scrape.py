8ecc1ba376ddd95b8e8ff7cacfe4e0897816df5e
Dave G <davegutz@users.noreply.github.com>
2024-01-15 10:23:07 -0500

    Update GUI_sqlite_scrape.py

--- GUI_sqlite_scrape.py
+++ GUI_sqlite_scrape.py
@@ -19,21 +19,8 @@ class IMDBdataBase:
         self.window.config(pady=20, padx=20, bg='#3a4470')
         self.window.resizable(False, False)
 
-        self.combo = ttk.Combobox(self.window, state="readonly",
-                                  values=['2022', '2023'], font=('Comic Sans MS', 10, 'bold'),
-                                  justify=CENTER)
-        self.combo.grid(row=7, column=0)
-        self.combo.current(0)
         self.year = datetime.now().year
 
-        # self.window = ThemedTk(theme='plastik')
-        # Set up second window
-        self.new_win = Toplevel(self.window)
-        self.new_win.title("Choose a film")
-        self.new_win.resizable(False, False)
-        self.new_win.config(pady=20, padx=20, bg='#378060')
-        self.new_win.withdraw()
-        self.new_win.protocol("WM_DELETE_WINDOW", self.on_closing)
         # IMDB API
         self.moviesDB = Cinemagoer()
         # Database
@@ -76,82 +63,52 @@ class IMDBdataBase:
         self.tree.heading('Date', text='Date', anchor=CENTER)
 
         self.scroll = Scrollbar(self.window, orient=VERTICAL)
-        self.scroll.grid(row=0, column=1, sticky=NS)
+        # self.scroll.grid(row=0, column=1, sticky=NS)
+        self.scroll.pack(side='top')
         self.tree.config(yscrollcommand=self.scroll.set)
         self.scroll.config(command=self.tree.yview)
         # Bind for tree double click item
         self.tree.bind("<Double-1>", self.OnDoubleClick)
         self.tree.bind("<Return>", self.OnDoubleClick)
 
-        self.tree.grid(row=0, column=0)
+        # self.tree.grid(row=0, column=0)
+        self.tree.pack(side='top')
         self.film_lbl = Label(text="Enter film:", font=('David', 15, 'bold'), bg='#3a4470', fg='#477bc9')
-        self.film_lbl.grid(row=1, column=0, pady=5)
+        # self.film_lbl.grid(row=1, column=0, pady=5)
+        self.film_lbl.pack(side='top')
         self.entry = Entry(width=30, font=('LilyUPC', 13, 'bold'), fg='#477bc9', bg='#2e3a4d')
-        self.entry.grid(row=2, column=0)
+        # self.entry.grid(row=2, column=0)
+        self.entry.pack(side='top')
         self.entry.focus()
         self.add_film_btn = Button(text="Add film", font=('LilyUPC', 13, 'bold'), bg='#7258db', width=25,
                                    command=self.add_film)
-        self.add_film_btn.grid(row=3, column=0, pady=8)
+        # self.add_film_btn.grid(row=3, column=0, pady=8)
+        self.add_film_btn.pack(side='top')
 
         self.file_lbl = Label(text="Enter file:", font=('David', 15, 'bold'), bg='#3a4470', fg='#477bc9')
-        self.file_lbl.grid(row=4, column=0, pady=5)
+        # self.file_lbl.grid(row=4, column=0, pady=5)
+        self.file_lbl.pack(side='top')
         self.file_entry = Entry(width=30, font=('LilyUPC', 13, 'bold'), fg='#477bc9', bg='#2e3a4d')
-        self.file_entry.grid(row=5, column=0)
+        # self.file_entry.grid(row=5, column=0)
+        self.file_entry.pack(side='top')
         self.file_entry.focus()
         self.add_file_btn = Button(text="Add file", font=('LilyUPC', 13, 'bold'), bg='#7258db', width=25,
                                    command=self.add_file)
-        self.add_file_btn.grid(row=6, column=0, pady=8)
+        # self.add_file_btn.grid(row=6, column=0, pady=8)
+        self.add_file_btn.pack(side='top')
 
 
         self.del_btn = Button(text="Delete record", font=('LilyUPC', 13, 'bold'), bg='#7258db', width=25,
                               command=self.delete_film)
-        self.del_btn.grid(row=7, column=0, pady=8)
+        # self.del_btn.grid(row=7, column=0, pady=8)
+        self.del_btn.pack(side='top')
         self.list_it()
 
-        self.new_btn = Button(text="List features", font=('LilyUPC', 13, 'bold'), bg='#7258db', width=25,
-                              command=self.win2)
-        self.new_btn.grid(row=8, column=0, pady=8)
-        self.list = Listbox(self.new_win, width=50, height=10, highlightthickness=0, font=('Andalus', 12, 'bold'),
-                            selectmode=SINGLE, bg='#044f19', fg='#65eb8a', selectbackground='#aed6b9',
-                            selectforeground='#68786d', selectborderwidth=3, activestyle=NONE)
-        self.list.grid(row=1, column=0)
-        self.scroll = Scrollbar(self.new_win, orient=VERTICAL)
-        self.scroll.grid(row=1, column=3, sticky=NS, rowspan=7)
-        self.scroll2 = Scrollbar(self.new_win, orient=HORIZONTAL)
-        self.scroll2.grid(row=2, column=0, sticky=EW)
-        self.list.config(yscrollcommand=self.scroll.set, xscrollcommand=self.scroll2.set)
-        self.scroll.config(command=self.list.yview)
-        self.scroll2.config(command=self.list.xview)
-        # Bind for ListBox double click item
-        self.list.bind('<Double-Button>', self.double_click)
-
-        self.combo.bind("<<ComboboxSelected>>", self.cambioCombo)
-
-        self.lbl = Label(self.new_win, text="Top 250 IMDB", font=('David', 20, 'bold'), bg='#378060', fg='#65eb8a')
-        self.lbl.grid(row=0, column=0)
-        self.label = Label(text=f'Viewed {len(self.tree.get_children())} features in {self.year}', bg='#3a4470',
-                           font=('David', 12, 'bold'), fg='white')
-        self.label.grid(row=9, column=0)
         self.window.title(f"Features viewed {self.year} -> ({len(self.tree.get_children())})")
 
         self.window.mainloop()
         self.conn.close()
         print("Connection finished")
-
-    def cambioCombo(self, event):
-        self.year = self.combo.get()
-        try:
-            self.list_it()
-        except sqlite3.OperationalError:
-            self.c.execute(f"CREATE TABLE if not exists Year{self.year}_Films(id integer PRIMARY KEY, title text, "
-                           "year integer, rating real, my_rating real, director text, actors text, generes text, summary text, cover text, date text)")
-            self.conn.commit()
-        finally:
-            self.list_it()
-
-        self.window.title(f"Features viewed {self.year} -> ({len(self.tree.get_children())})")
-        self.label.config(text=f'Viewed {len(self.tree.get_children())} features in {self.year}', bg='#3a4470',
-                          font=('David', 12, 'bold'), fg='white')
 
     def double_click(self, event):
         """Called when user double clicks element from ListBox"""
@@ -160,15 +117,6 @@ class IMDBdataBase:
         self.new_win.clipboard_append(self.list.get(self.list.curselection()))
         self.entry.insert(0, self.list.get(self.list.curselection()))
         messagebox.showinfo(title="Info", message=self.get_movie_info())
-
-    def win2(self):
-        """Called when user press 'List features' button to show up second window"""
-        self.list.delete(0, 'end')
-        self.window.grab_set()
-        self.new_win.deiconify()
-
-        top = self.moviesDB.get_top250_movies()
-        self.list.insert('end', *top)
 
     def on_closing(self):
         """Called when user closes second window"""
@@ -256,8 +204,6 @@ Viewed: {item['values'][8]}
                     self.list_it()
                 except UnboundLocalError:
                     pass
-            self.label.config(text=f'Viewed {len(self.tree.get_children())} features in {self.year}', bg='#3a4470',
-                              font=('David', 12, 'bold'), fg='white')
             self.window.title(f"Features viewed {self.year} -> ({len(self.tree.get_children())})")
             self.conn.commit()
 
@@ -282,8 +228,6 @@ Viewed: {item['values'][8]}
             print("Index Error")
         self.conn.commit()
         self.list_it()
-        self.label.config(text=f'Viewed {len(self.tree.get_children())} features in {self.year}', bg='#3a4470',
-                          font=('David', 12, 'bold'), fg='white')
         self.window.title(f"Features viewed {self.year} -> ({len(self.tree.get_children())})")
 
     def get_movie_info(self):
