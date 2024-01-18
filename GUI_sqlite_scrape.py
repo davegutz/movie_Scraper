80046d00797fc0e7a973038753eb359909b010ca
Dave G <davegutz@users.noreply.github.com>
2024-01-18 05:16:10 -0500

    Update GUI_sqlite_scrape.py

--- GUI_sqlite_scrape.py
+++ GUI_sqlite_scrape.py
@@ -484,25 +484,16 @@ class IMDBdataBase:
                 print(f"error {i=}")
             print(f"i at end {i=}")
         print(f"{select_list=}")
-        combo = ttk.Combobox(state="readonly", values=select_list)
-        new_choice = combo.get()
+        # combo = ttk.Combobox(state="readonly", values=select_list)
+        # new_choice = combo.get()
         window = tk.Tk()
         window.title('Combobox')
-        window.geometry('500x250')
-        # label text for title
-        ttk.Label(window, text="GFG Combobox Widget",
-                  background='green', foreground="white",
+        window.geometry('800x250')
+        ttk.Label(window, text=title + '(' + str(year) + ')', background='green', foreground="white",
                   font=("Times New Roman", 15)).grid(row=0, column=1)
-
-        # label
-        ttk.Label(window, text="Select the Month :",
-                  font=("Times New Roman", 10)).grid(column=0,
-                                                     row=5, padx=10, pady=25)
-
-        # Combobox creation
+        ttk.Label(window, text="Select the Film :", font=("Times New Roman", 10)).grid(column=0, row=5, padx=10, pady=25)
         n = tk.StringVar()
         film_chosen = ttk.Combobox(window, width=27, textvariable=n)
-        # Adding combobox drop down list
         film_chosen['values'] = select_list
         film_chosen.grid(column=1, row=5)
         film_chosen.current()
