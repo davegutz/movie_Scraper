b8ed22ab55d32924b1ac6caebe205f73f80b125c
Dave G <davegutz@users.noreply.github.com>
2024-06-02 05:48:09 -0400

    Update GUI_sqlite_scrape.py

--- GUI_sqlite_scrape.py
+++ GUI_sqlite_scrape.py
@@ -1,6 +1,6 @@
 #! /bin/sh
 "exec" "`dirname $0`/venv/bin/python3" "$0" "$@"
-#! /users/daveg/Documents/GitHub/myStateOfCharge/SOC_Particle/py/venv/bin/python3.12
+##! /users/daveg/Documents/GitHub/myStateOfCharge/SOC_Particle/py/venv/bin/python3.12
 #  Manage movie database in conjunction with 'DB Browser for SQLite'
 #  Run in PyCharm
 #     or
