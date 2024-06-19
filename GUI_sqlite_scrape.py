508af056dbcbc60536969e9727bb06efb7523b53
Dave G <davegutz@alum.mit.edu>
2024-06-19 12:45:07 -0400

    Update GUI_sqlite_scrape.py

--- GUI_sqlite_scrape.py
+++ GUI_sqlite_scrape.py
@@ -44,6 +44,7 @@ import tkinter.messagebox
 import sqlite3
 import csv
 import imdb
+from PIL.ImageTk import PhotoImage
 from imdb import Cinemagoer  # install cinemagoer
 from datetime import datetime
 from PIL import ImageTk, Image  # install pillow
@@ -210,6 +211,7 @@ class IMDBdataBase:
         self.root = tk.Tk()
         self.root.config(pady=20, padx=20, bg=bg_color)
         self.root.resizable(False, False)
+        self.root.iconphoto(False, tk.PhotoImage(file = './popcorn.png'))
         self.top_frame = tk.Frame(self.root)
         self.top_frame.pack(side='top', expand=True, fill='both')
         self.mid_frame = tk.Frame(self.root)
