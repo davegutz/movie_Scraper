2b61dbf658c87710f1629a42e4a612fef6417bf7
Dave G <davegutz@alum.mit.edu>
2026-09-13 17:58:31 -0400

    Update resample_library.py

--- resample_library.py
+++ resample_library.py
@@ -1712,4 +1712,4 @@ if __name__ == "__main__":
     if len(sys.argv) > 1:
         main()
     else:
-        main("--max-files 5 --sort-by size_desc")
+        main("--max-files 10 --sort-by size_desc")
